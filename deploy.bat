8ba2c1ce6e2213d768ea2f7931f9624fe98b8c2d
Ahmed Hassan <ahmadmhasann@gmail.com>
2024-10-13 23:09:08 +0300

    deploy

--- deploy.bat
+++ deploy.bat
@@ -2,25 +2,24 @@
 setlocal
 
 set /p message="Enter commit message (default: 'deploy'): "
-
 if "%message%"=="" (
     set message=deploy
 )
 
+echo --------------------- Building Web
 cd ../fire_alarm_system
-
 call flutter build web
 
+echo --------------------- Copying Files
 cd ../smart-fire-system.github.io
-
 xcopy /s /y /i "..\fire_alarm_system\build\web\*" .
 
+echo --------------------- Git Commit
 git add .
-
 git commit -m "%message%"
 
+echo --------------------- Git Push
 git push
-
 pause
 
 endlocal
