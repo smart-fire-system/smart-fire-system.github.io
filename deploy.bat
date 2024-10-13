0fb6eb91e7f9b90cd4d491e52f0f707277509fd7
Ahmed Hassan <ahmadmhasann@gmail.com>
2024-10-13 23:06:29 +0300

    deploy

--- deploy.bat
+++ deploy.bat
@@ -1,35 +1,26 @@
 @echo off
 setlocal
 
-REM Step 1: Navigate to the fire_alarm_system directory
-cd ../fire_alarm_system
+set /p message="Enter commit message (default: 'deploy'): "
 
-REM Step 2: Build the Flutter web app
-call flutter build web
-
-REM Step 3: Navigate to the smart-fire-system.github.io directory
-cd ../smart-fire-system.github.io
-
-REM Step 4: Copy all files from fire_alarm_system/build/web/*.* to the current directory with force replace
-xcopy /s /y /i "..\fire_alarm_system\build\web\*" .
-
-REM Step 5: Add all files to git
-git add .
-
-REM Step 6: Check if a commit message was passed to the script
-if "%~1"=="" (
-    set "message=deploy"
-) else (
-    set "message=%~1"
+if "%message%"=="" (
+    set message=deploy
 )
 
-REM Step 7: Commit with the message
+cd ../fire_alarm_system
+
+call flutter build web
+
+cd ../smart-fire-system.github.io
+
+xcopy /s /y /i "..\fire_alarm_system\build\web\*" .
+
+git add .
+
 git commit -m "%message%"
 
-REM Step 8: Push the changes to the repository
 git push
 
-REM Step 9: Pause the script at the end
 pause
 
 endlocal
